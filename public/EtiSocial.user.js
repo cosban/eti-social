4c8e9342fca58017a9ff8c0842ad17fae5d05898
william-bishop <admin@the402.net>
2016-04-07 17:13:21 -0700

    bumping version

--- public/EtiSocial.user.js
+++ public/EtiSocial.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ETI Social
 // @namespace    http://tampermonkey.net/
-// @version      0.0.9.7
+// @version      0.0.9.8
 // @description  Social ETI experience
 // @author       - s otaku -
 // @match        http://boards.endoftheinter.net/showmessages.php*
