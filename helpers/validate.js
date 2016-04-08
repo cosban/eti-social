652826e6d531b410ea05636a2a25ee551ac3203c
william-bishop <admin@the402.net>
2016-04-07 21:49:40 -0700

    dev mode

--- helpers/validate.js
+++ helpers/validate.js
@@ -5,6 +5,11 @@ module.exports = {
         return new Promise(function (res, rej) {
             var url = 'http://boards.endoftheinter.net/scripts/login.php?username=' + user + '&ip=' + ip;
 
+            if(process.env.DEV) {
+                console.log('Skipping validation [dev env]');
+                return res(true);
+            }
+
             request(url, function (error, response, body) {
                 try {
                     if (!error && response.statusCode == 200 && body && body.match(new RegExp('1:' + user))) {
